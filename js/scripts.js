a9a8eb07da79426bcc191e520e35d5370be40a09
Arturo Aguilar <arturo_aguilar_t@hotmail.com>
2019-10-23 02:23:10 -0500

    Subir portafolio

--- js/scripts.js
+++ js/scripts.js
@@ -8,28 +8,34 @@ const Proyecto = {
     data: function () {
         return {
             proyectos: [
-              {id:1,nombre: 'Web',cliente:'Bright Loritos',foto:'/proyectos/cv/img/proyecto.png',foto_portada:'/proyectos/cv/img/proyecto_portada.png', desc:'Desc proyecto 0',meta_pro:'meta 0' },
-              {id:2,nombre: 'vivagym ',cliente:'VivaGYM',foto:'/proyectos/cv/img/proyecto.png',foto_portada:'/proyectos/cv/img/proyecto_portada.png', desc:'Desc proyecto 1',meta_pro:'meta 1' },
-              {id:3,nombre: 'vivagym User',cliente:'VivaGYM',foto:'/proyectos/cv/img/proyecto.png',foto_portada:'/proyectos/cv/img/proyecto_portada.png', desc:'Desc proyecto 2' ,meta_pro:'meta 2'},
-              {id:4,nombre: 'Sistema de reservas',cliente:'Pawadin',foto:'/proyectos/cv/img/proyecto.png',foto_portada:'/proyectos/cv/img/proyecto_portada.png', desc:'Desc proyecto 3' ,meta_pro:'meta 3'},
-              {id:5,nombre: 'pawadin school',cliente:'Pawadin',foto:'/proyectos/cv/img/proyecto.png',foto_portada:'/proyectos/cv/img/proyecto_portada.png', desc:'Desc proyecto 4' ,meta_pro:'meta 4' },
-              {id:6,nombre: 'CRM',cliente:'Inet',foto:'/proyectos/cv/img/proyecto.png',foto_portada:'/proyectos/cv/img/proyecto_portada.png', desc:'Desc proyecto 5' ,meta_pro:'meta 5' },
-              {id:7,nombre: 'App Reservas',cliente:'Pawadin',foto:'/proyectos/cv/img/proyecto.png',foto_portada:'/proyectos/cv/img/proyecto_portada.png', desc:'Desc proyecto 6' ,meta_pro:'meta 6' }
+              {id:1,nombre: 'Web',cliente:'Bright Loritos',foto:'/proyectos/cv/img/portadas/brightloritos_prin.png',foto_portada:'/proyectos/cv/img/portadas/bright_lori_port.png', desc:'Brightloritos, es una academia de idiomas enfocada en los niños, teniendo en cuenta siempre a la diversión como parte fundamental en el aprendizaje de sus alumnos. Debido a las distintas sucursales, distintos horarios y distintas edades de los alumnos se tuvieron que realizar un procesos de diseño de la información de la mano con el cliente segun las prioridades y necesidades del negocio. El desarrollo de esta página, se hizo en paralelo con un centro de aprendizaje online para los profesores de Brightloritos.',meta_pro:'Arquitectura de la información / Design Thinking / User Experience / Customer Journey Map' },
+              {id:2,nombre: 'Sistema para voluntarios',cliente:'VivaGYM',foto:'/proyectos/cv/img/portadas/vivagymejprin.png',foto_portada:'/proyectos/cv/img/portadas/vivagymej.png', desc:'Desc proyecto 1',meta_pro:'meta 1' },
+              {id:3,nombre: 'Sistema de entregas de departamentos',cliente:'VivaGYM',foto:'/proyectos/cv/img/portadas/vivagymadminprin.png',foto_portada:'/proyectos/cv/img/portadas/vivagymadmin.png', desc:'Desc proyecto 2' ,meta_pro:'meta 2'},
+              {id:4,nombre: 'Sistema de reservas',cliente:'Pawadin',foto:'/proyectos/cv/img/portadas/pawadinprin.png',foto_portada:'/proyectos/cv/img/portadas/port_pawadin.png', desc:'Desc proyecto 3' ,meta_pro:'meta 3'},
+              {id:5,nombre: 'pawadin school',cliente:'Pawadin',foto:'/proyectos/cv/img/portadas/paw_school_prin.png',foto_portada:'/proyectos/cv/img/portadas/pawschool_port.png', desc:'Desc proyecto 4' ,meta_pro:'meta 4' },
+              {id:6,nombre: 'CRM',cliente:'Inet',foto:'/proyectos/cv/img/portadas/inet_principal.png',foto_portada:'/proyectos/cv/img/portadas/inet_portada.png', desc:'Desc proyecto 5' ,meta_pro:'meta 5' },
+              {id:7,nombre: 'App Reservas',cliente:'Pawadin',foto:'/proyectos/cv/img/portadas/app_paw_prin.png',foto_portada:'/proyectos/cv/img/portadas/apw_app_port.png', desc:'Desc proyecto 6' ,meta_pro:'meta 6' }
             ]
           }
     },
     template: `
-      <div class="proyecto">
-        <div v-for="proyecto in proyectos" class="col">
-        <div v-if="proyecto.id==$route.params.id " >
-        <h2>Proyecto: {{ proyecto.nombre }}</h2>
-        <h3>Cliente: {{ proyecto.cliente }}</h3>
-        <img v-bind:src="proyecto.foto_portada" class="img-fluid">
-        <p>{{ proyecto.meta_pro }}</p>
+      <div class="proyecto_detalle">
+        <div v-for="proyecto in proyectos" class="row">
+
+        <div v-if="proyecto.id==$route.params.id " class="col-12">
+        <img v-bind:src="proyecto.foto_portada" class="img-fluid portada_proyecto">
+        <div class="titulo_detalle" >Proyecto: <b >{{ proyecto.nombre }}</b></div>
+        <div class="sub_titulo_detalle" >Cliente: <b >{{ proyecto.cliente }}</b></div>
+        
+        <div>
+        <p style="opacity: 0.57;">{{ proyecto.meta_pro }}</p>
         <p>{{ proyecto.desc }}</p>
         </div>
+        
         </div>
-        <router-view></router-view>
+
+        </div>
+ 
       </div>
     `
   }
@@ -38,6 +44,11 @@ const Proyecto = {
         return {
         nombre: 'Arturo Aguilar',
         profesion:'UX/CX Diseño de productos digitales',
+        mail:'aatbjj2@gmail.com',
+        linkedin:"https://linkedin.com/in/arturoat/",
+        facebook:"https://www.facebook.com/arturo.aguilartobies",
+        instagram:"https://www.instagram.com/carcochacosmica/",
+        despedida:'Si deseas contactarme para ayudarte en algún proyecto, no dudes en escribirme.',
         descripcion:'Mi experiencia viene del mundo del desarrollo de productos y servicios digitales, e investigación de usuarios. Me encanta involucrarme en campos de la innovación, transformación digital y diseño de experiencias, tanto en el entorno digital como en el físico. Estoy seguro que el centro de todo buen diseño, tiene que ver con el entendimiento profundo de las personas.',
         competencias: [
           { nombre: 'User Experience', tipo:1 },
@@ -58,13 +69,13 @@ const Proyecto = {
           { nombre: 'wordpress y woocommerce',tipo:3}
         ],
         proyectos: [
-          {id:1,nombre: 'Web',cliente:'Bright Loritos',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 0',meta_pro:'meta 0' },
-          {id:2,nombre: 'vivagym ',cliente:'VivaGYM',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 1',meta_pro:'meta 1' },
-          {id:3,nombre: 'vivagym User',cliente:'VivaGYM',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 2' ,meta_pro:'meta 2'},
-          {id:4,nombre: 'Sistema de reservas',cliente:'Pawadin',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 3' ,meta_pro:'meta 3'},
-          {id:5,nombre: 'pawadin school',cliente:'Pawadin',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 4' ,meta_pro:'meta 4' },
-          {id:6,nombre: 'CRM',cliente:'Inet',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 5' ,meta_pro:'meta 5' },
-          {id:7,nombre: 'App Reservas',cliente:'Pawadin',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 6' ,meta_pro:'meta 6' }
+          {id:1,nombre: 'Web',cliente:'Bright Loritos',foto:'/proyectos/cv/img/portadas/brightloritos_prin.png', desc:'Desc proyecto 0',meta_pro:'meta 0' },
+          {id:2,nombre: 'Sistema para voluntarios ',cliente:'VivaGYM',foto:'/proyectos/cv/img/portadas/vivagymejprin.png', desc:'Desc proyecto 1',meta_pro:'meta 1' },
+          {id:3,nombre: 'Sistema de entregas de departamentos',cliente:'VivaGYM',foto:'/proyectos/cv/img/portadas/vivagymadminprin.png', desc:'Desc proyecto 2' ,meta_pro:'meta 2'},
+          {id:4,nombre: 'Sistema de reservas',cliente:'Pawadin',foto:'/proyectos/cv/img/portadas/pawadinprin.png', desc:'Desc proyecto 3' ,meta_pro:'meta 3'},
+          {id:5,nombre: 'pawadin school',cliente:'Pawadin',foto:'/proyectos/cv/img/portadas/paw_school_prin.png', desc:'Desc proyecto 4' ,meta_pro:'meta 4' },
+          {id:6,nombre: 'CRM',cliente:'Inet',foto:'/proyectos/cv/img/portadas/inet_principal.png', desc:'Desc proyecto 5' ,meta_pro:'meta 5' },
+          {id:7,nombre: 'App Reservas',cliente:'Pawadin',foto:'/proyectos/cv/img/portadas/app_paw_prin.png', desc:'Desc proyecto 6' ,meta_pro:'meta 6' }
         ],
         estudios: [
           { nombre: 'Master en Neuromarketing',lugar:'link foto 1', desc:'Desc estudios 1' },
@@ -80,50 +91,87 @@ const Proyecto = {
       }, template: `<div> <div class="row seccion hero">   
   <div class="col-12 nombre">{{ nombre }}</div> 
    <div class="col-12 profesion">{{ profesion}}</div>
-   
-
+   <div class="col-12 img_redes_contenedor">
+   <a v-bind:href="facebook"><img class="img_redes" src="img/icon/facebook.png"/></a>
+   <a v-bind:href="linkedin"><img class="img_redes" src="img/icon/in.png"/></a>
+   <a v-bind:href="instagram"><img class="img_redes" src="img/icon/instagram.png"/></a>
+   </div>
      </div>
      <div class="row seccion sobremi">
      <div class="col-12 titulo-seccion">Sobre mí</div> 
      <div class="col-12">
      <p class="col-12  descripcion">{{ descripcion }}</p>
     </div>
+
      </div>
+
+     <div class="row seccion proyectos">
+     <div class="col-12 titulo-seccion ">Proyectos</div> 
+     
+     
+     
+     <div v-for="proyecto in proyectos" v-on:click="say(proyecto.id)" class="col-4 img_container" style="cursor: pointer; margin-top:10px;margin-bottom:10px;">
+     
+     <img v-bind:src="proyecto.foto" class="img-fluid image">
+     <div class="overlay">
+     <div class="text">{{ proyecto.cliente }}<br>{{ proyecto.nombre }}    </div>
+     </div>
+      </div>
+     
+     </div>
+
 <div class="row seccion habilidades">
 <div class="col-12 titulo-seccion">Habilidades</div> 
-<div class="col-4">
+<div class="col-12">
+
+<div class="row" style="padding-top:3%;padding-bottom:3%;background-color: #F5F5F5;">
+<div class="col-2" style="text-align:right">
+<img src="img/icon/web_design.png" style="max-width:70px">
+</div>
+<div class="col-10" style="text-align:left">
 <span class="mini-item" v-for="competencia in competencias"  v-if="competencia.tipo==1 ">
    {{ competencia.nombre }}
  </span>
  </div>
- <div class="col-4">
+ </div>
+ 
+ 
+ <div class="row" style="padding-top:3%;padding-bottom:3%;">
+ <div class="col-2" style="text-align:right">
+ <img src="img/icon/management.png" style="max-width:70px">
+ </div>
+ <div class="col-10" style="text-align:left">
 <span class="mini-item" v-for="competencia in competencias"  v-if="competencia.tipo==2 ">
    {{ competencia.nombre }}
  </span>
  </div>
- <div class="col-4">
+ </div>
+ 
+ <div class="row" style="padding-top:3%;padding-bottom:3%;background-color: #F5F5F5;">
+ <div class="col-2" style="text-align:right">
+ <img src="img/icon/web_developement.png" style="max-width:70px">
+ </div>
+ <div class="col-10" style="text-align:left">
 <span class="mini-item" v-for="competencia in competencias"  v-if="competencia.tipo==3 ">
    {{ competencia.nombre }}
  </span>
  </div>
-
-</div>
-<div class="row seccion">
-   <span class="col-12 titulo-seccion">Proyectos</span> 
-<div v-for="proyecto in proyectos" v-on:click="say(proyecto.id)" class="col-4" style="cursor: pointer;">
-{{ proyecto.cliente }}-{{ proyecto.nombre }}    
-<img v-bind:src="proyecto.foto" class="img-fluid">
-  
  </div>
-
+ </div>
 </div>
 
-<div class="seccion">
-   <span class="col-12 titulo-seccion">Estudios</span> 
-<li v-for="estudio in estudios">
-  <b>{{ estudio.nombre }}</b>- {{ estudio.lugar }}-{{ estudio.desc }}
- </li>
-</div></div> ` 
+
+<div class="row seccion contacto"  >
+   <div class="col-12 titulo-seccion">Conversemos</div> 
+   <div class="col-12">
+<p>{{ despedida }}</p>
+</div>
+
+<div class="col-12" style="padding-top:20px;">
+<a class="btnmail" v-bind:href="[\'mailto:\' + mail]">Enviar Mensaje</a>
+</div>
+</div>
+</div> ` 
 
 }
   const UserHome = { template: `<div>Home</div>` }
@@ -192,7 +240,7 @@ const app = new Vue({
       proyectos: [
         { id:1,nombre: 'Web',cliente:'Bright Loritos',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 0',meta_pro:'meta 0' },
         { id:2,nombre: 'vivagym ',cliente:'VivaGYM',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 1',meta_pro:'meta 1' },
-        { id:3,nombre: 'vivagym User',cliente:'VivaGYM',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 2' ,meta_pro:'meta 2'},
+        { id:3,nombre: 'vivagym User',cliente:'VivaGYM',foto:'/proyectos/cv/img/portadas/vivagymadminprin.png', desc:'Desc proyecto 2' ,meta_pro:'meta 2'},
         { id:4,nombre: 'Sistema de reservas',cliente:'Pawadin',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 3' ,meta_pro:'meta 3'},
         { id:5,nombre: 'pawadin school',cliente:'Pawadin',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 4' ,meta_pro:'meta 4' },
         { id:6,nombre: 'CRM',cliente:'Inet',foto:'/proyectos/cv/img/proyecto.png', desc:'Desc proyecto 5' ,meta_pro:'meta 5' },
