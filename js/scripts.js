730a00379930b75bacaa785249729b1a289d136a
Arturo Aguilar <arturo_aguilar_t@hotmail.com>
2019-11-12 04:45:18 -0500

    agregando slick detalle 5

--- js/scripts.js
+++ js/scripts.js
@@ -122,6 +122,12 @@ const Proyecto = {
     <div   class="carousel-item active">
       <img class="d-block w-100"  src="/img/proyectos/pawschool01.png" alt="First slide">
     </div>
+    <div   class="carousel-item ">
+    <img class="d-block w-100"  src="/img/proyectos/pawschool02.png" alt="First slide">
+  </div>
+  <div   class="carousel-item ">
+  <img class="d-block w-100"  src="/img/proyectos/pawschool03.png" alt="First slide">
+</div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
