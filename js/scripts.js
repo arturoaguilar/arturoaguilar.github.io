76ef9920a80d063ea8e6edc0911281440286dfd9
Arturo Aguilar <arturo_aguilar_t@hotmail.com>
2019-11-12 04:20:00 -0500

    agregando slick detalle 5

--- js/scripts.js
+++ js/scripts.js
@@ -87,53 +87,11 @@ const Proyecto = {
         <p>{{ proyecto.desc }}</p>
         
         
-        <div class="your-class">
-        <div  v-for="proyectoimg in proyecto.fotos" >
-        <img v-bind:src="proyectoimg.foto"  style="width:300px;">
-         </div>
-         </div>
-        </div>
-        
-        </div>
-
-        </div>
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-          Launch demo modal
-        </button>
-        
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                ...
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <!--slider-->
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="/img/proyectos/pawschool01.png" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="/img/proyectos/pawschool02.png" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="/img/proyectos/pawschool03.png" alt="Third slide">
+    <div v-for="proyectoimg in proyecto.fotos"  class="carousel-item ">
+      <img class="d-block w-100" v-bind:src="proyectoimg.foto"  alt="First slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -145,6 +103,17 @@ const Proyecto = {
     <span class="sr-only">Next</span>
   </a>
 </div>
+   <!-- Fin slider -->
+     
+        </div>
+        
+        </div>
+
+        </div>
+
+        
+
+
 
         
 
