8091f6b43875a9487618e8ab641f932964de258f
Arturo Aguilar <arturo_aguilar_t@hotmail.com>
2019-11-12 00:57:08 -0500

    agregando slick detalle 5

--- js/scripts.js
+++ js/scripts.js
@@ -77,7 +77,7 @@ const Proyecto = {
         <div class="your-class">
         <div  v-for="proyectoimg in proyecto.fotos" >
 
-        <img v-bind:src="proyectoimg.foto" class="img-fluid image">
+        <img v-bind:src="proyectoimg.foto" class="img-fluid image" style="width:300px;">
    
          </div>
          </div>
