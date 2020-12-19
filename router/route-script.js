2fc3cf39b4a2b3b4e3d59458061bf4f1d77cb513
arturoaguilar <arturo_aguilar_t@hotmail.com>
2020-12-19 05:23:56 -0500

    Update route-script.js
    arreglar la referencia al componente home

--- router/route-script.js
+++ router/route-script.js
@@ -3,7 +3,7 @@
 
 // 1. Define route components.
 // These can be imported from other files
-const Foo = { template: '#home' }
+const Foo = { template: '#/home' }
 const Bar = { template: '<div>bar</div>' }
 
 // 2. Define some routes
