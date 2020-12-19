b82666bb04873ff304229797d239887fd9cc1fba
Arturo Aguilar <aatbjj2@gmail.com>
2020-12-19 05:16:50 -0500

    se modificó la fuente de un componente

--- router/home.vue
+++ router/home.vue
@@ -2,7 +2,9 @@
   <div class="checkbox-wrapper" @click="check">
     <div :class="{ checkbox: true, checked: checked }"></div>
     <div class="title">{{ title }}</div>
+    hola hola hola
   </div>
+  chau chau chau 
 </template>
 <script>
 export default {
