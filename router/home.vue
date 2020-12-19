e907a6f66effd9620ae0fee2d1ebeb7d9b0bc9fe
Arturo Aguilar <aatbjj2@gmail.com>
2020-12-19 07:04:14 -0500

    se realizaronr cambios a más archivos

--- router/home.vue
+++ router/home.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="checkbox-wrapper" @click="check">
-    <div :class="{ checkbox: true, checked: checked }"></div>
-    <div class="title">{{ title }}</div>
     hola hola hola
-  </div>
   chau chau chau 
 </template>
-<script>
-export default {
-  data() {
-    return { checked: false, title: 'Check me' }
-  },
-  methods: {
-    check() { this.checked = !this.checked; }
-  }
-};
-</script>
+
