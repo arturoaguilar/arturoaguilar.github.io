b97935a9d807bde0fce252061af164c3efb75aa9
Arturo Aguilar <aatbjj2@gmail.com>
2020-12-19 00:35:45 -0500

    nueva versión del código con routing 12

--- router/route-script.js
+++ router/route-script.js
@@ -1,31 +1,31 @@
+// Make sure to _use_ the router instance to make the
+// 0. If using a module system, call Vue.use(VueRouter)
+
 // 1. Define route components.
 // These can be imported from other files
-const Home = { template: '<div>Home</div>' }
-const About = { template: '<div>About</div>' }
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
 
 // 2. Define some routes
-// Each route should map to a component.
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// Vue.extend(), or just a component options object.
 // We'll talk about nested routes later.
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
 ]
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
-const router = VueRouter.createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  history: VueRouter.createWebHashHistory(),
-  routes, // short for `routes: routes`
+const router = new VueRouter({
+  routes
 })
 
-// 5. Create and mount the root instance.
-const app = Vue.createApp({})
-// Make sure to _use_ the router instance to make the
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
 // whole app router-aware.
-app.use(router)
-
-app.mount('#app')
-
-// Now the app has started!
+const app = new Vue({
+  router
+}).$mount('#app')
