36f2dcdb55e541a7fcd614571f640449ced0b77e
home <home@MacBook-Pro-de-home.local>
2020-12-22 15:53:44 -0500

    Se agregó la pantalla de detalle y se incluyó en el router

--- router/detail.vue.js
+++ router/detail.vue.js
@@ -1,18 +1,7 @@
-const Hero = {
+const Projects = {
     data() {
     return {
      mensaje:"Taksumaqui sempucriaco",
-     portfolo:{
-	    name: "Arturo Aguilar",
-	    profession:[
-	    {name:"User Experience ",link:"url UX",state:1,icono:"url imagen ícono A "},
-	    {name:"Diseño de producto", link:" Link Diseño de producto", state:1,icono:"url imagen B "},
-	    {name:"User Research", link: "Link User Research",state:1,icono:"url imagen C"},
-	      {name:"Frontend Developer", link: "Link Fron end",state:1,icono:"url imagen D"}
-	    ],  
-	      message: "Bienvenido a mi portafolio"
-	  
-	    }, 
      projects:
 	    [
 	      {name:"Sistema de entregas de departamepentos",
