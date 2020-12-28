c5c5140a797ed420a0bf5bf7d351ee4e726488a8
home <home@MacBook-Pro-de-home.local>
2020-12-28 04:32:09 -0500

    Se agregó el parámetro routing

--- router/detail.vue.js
+++ router/detail.vue.js
@@ -48,7 +48,7 @@ const Projects = {
        }
      },
   created() {
-     const selection = this.projects.find(element => element.id = "001");
+     const selection = this.projects.find(element => element.id = this.$route.params.name);
     // `this` points to the vm instance
     console.log('SE CREA LA APLICACION');
     console.log(selection);// => "count is: 1"
