2343fde5daee723a768d1a606ef3c9ff6945d20e
HOME <home@MacBook-Pro-de-HOME.local>
2021-01-06 03:56:15 -0500

    Se agregaron las habilidades

--- home.vue.js
+++ home.vue.js
@@ -12,7 +12,21 @@ const Hero = {
 	    ],  
 	      message: "Me encanta el desarrollo de tecnologías, la innovación, la investigación de usuarios y escribir"
 	  
-	    }, 
+	    },
+    abilities:[
+      {id:"001",name:"User Research":type:1,typeName:"Product Design",level:3},
+      {id:"002",name:"Design Thinking":type:1,typeName:"Product Design",level:3},
+      {id:"003",name:"Lean Ux":type:1,typeName:"Product Design",level:3},
+      {id:"004",name:"Google Design Sprint":type:1,typeName:"Product Design",level:3},
+      {id:"005",name:"Scrum":type:1,typeName:"Product Design",level:3},
+      {id:"006",name:"Gamification":type:1,typeName:"Product Design",level:3},
+      {id:"007",name:"Html":type:2,typeName:"Frontend Development",level:3},
+      {id:"008",name:"Javascript":type:2,typeName:"Frontend Development",level:3},
+      {id:"009",name:"Angular 9/10":type:2,typeName:"Frontend Development",level:3},
+      {id:"010",name:"Vue3":type:2,typeName:"Frontend Development",level:3},
+      {id:"011",name:"CSS":type:2,typeName:"Frontend Development",level:3},
+      {id:"012",name:"Google Firestore":type:2,typeName:"Frontend Development",level:3}
+    ],
      projects:
 	    [
 	      {id:"001",name:"Sistema de entregas de departamepentos",
@@ -28,23 +42,29 @@ const Hero = {
   },
   template: `
   <div>
-          Hola, soy  {{ portfolio.name }}, y {{ portfolio.message }} 
+ <section class="hero">
+ Hola, soy  {{ portfolio.name }}, y {{ portfolio.message }} 
   <div> 
       <span class="profession__item" v-for="profession in portfolio.profession">
     {{ profession.name }}
      </span>
       </div>
+</section>
 
-<div class="projects row">
+<section class="projects row">
 <div class="projects__block col-xs-12 col-md-4" v-for="project in projects">
-
 <router-link class="projects__block__link" :to="'project/'+project.id">
 <img class="projects__block__img hvr-grow" v-bind:src="project.portImg" />
 <span class="projects__block__title">{{ project.name}}</span>
 </router-link>
+</div>
+</section>
 
-</div>
-</div>
+<section class="abilities">
+<span class="ability__block col-xs-12 col-md-4" v-for="ability in abilities">
+{{ability.name }}
+</span>
+</section>
 
   </div>
   `
