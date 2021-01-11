c8be51b37dc522ae04b262728295c4b21c0893e6
HOME <home@MacBook-Pro-de-HOME.local>
2021-01-11 18:27:42 -0500

    Se agegó la etiqueta span a los div de los títulos de sección

--- home.vue.js
+++ home.vue.js
@@ -83,8 +83,8 @@ Hola, soy <h3> {{ portfolio.name }}</h3>
 </section>
 
 <section class="projects">
-<div class="section__title--projects row ">
-<span class="col-xs-offset-8" >Proyectos </span>
+<div class="row ">
+<span class="section__title--projects col-xs-offset-8" >Proyectos </span>
 </div>
 <div class="row">
 <div class="projects__block col-xs-12 col-md-4" v-for="project in projects">
@@ -97,8 +97,8 @@ Hola, soy <h3> {{ portfolio.name }}</h3>
 </section>
 
 <section class="abilities">
-<div class="section__title--abilities row">
-<span  class="col-xs-offset-4" >habilidades </span>
+<div class=" row">
+<span  class="section__title--abilities col-xs-offset-4" >habilidades </span>
 </div>
 <div class="row">
 <div class="col-md-6">
@@ -122,8 +122,8 @@ Hola, soy <h3> {{ portfolio.name }}</h3>
 
 
 <section class="tools">
-<div class="section__title--tools row">
-<span class="col-xs-offset-8" > Herramientas </span>
+<div class="row">
+<span class="section__title--tools col-md-offset-8" > Herramientas </span>
 </div>
 <span class="ability__block col-xs-12 col-md-4" v-for="tool in tools">
 {{tool.name }}
