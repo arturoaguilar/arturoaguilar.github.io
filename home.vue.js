ca6c8b303d9c32b97b7049675cb126185fb924f3
HOME <home@MacBook-Pro-de-HOME.local>
2021-02-26 19:48:07 -0500

    Se añadió información a User Experience

--- home.vue.js
+++ home.vue.js
@@ -17,12 +17,14 @@ const Hero = {
         { id: "001", name: "User Research", type: 1, typeName: "Product Design", level: 3 },
         { id: "002", name: "Design Thinking", type: 1, typeName: "Product Design", level: 3 },
         { id: "003", name: "Lean Ux", type: 1, typeName: "Product Design", level: 3 },
+        { id: "003", name: "Lean Startup", type: 1, typeName: "Product Design", level: 3 },
         { id: "004", name: "Google Design Sprint", type: 1, typeName: "Product Design", level: 3 },
         { id: "005", name: "Scrum", type: 1, typeName: "Product Design", level: 3 },
         { id: "006", name: "Gamification", type: 1, typeName: "Product Design", level: 3 },
         { id: "007", name: "Html", type: 2, typeName: "Frontend Development", level: 3 },
         { id: "008", name: "Javascript", type: 2, typeName: "Frontend Development", level: 3 },
         { id: "009", name: "Angular 9/10", type: 2, typeName: "Frontend Development", level: 3 },
+        { id: "009", name: "Arquitectura de la Información", type: 2, typeName: "Frontend Development", level: 3 },
         { id: "010", name: "Vue3", type: 2, typeName: "Frontend Development", level: 3 },
         { id: "011", name: "CSS", type: 2, typeName: "Frontend Development", level: 3 },
         { id: "012", name: "Google Firestore", type: 2, typeName: "Frontend Development", level: 3 },
@@ -215,14 +217,14 @@ const Hero = {
 <div  class="abilities__inner row">
 
 <div data-aos="fade-up"  data-aos-duration="1000" class="abilities__block col-md-6 col-xs-12">
-<h2 class="col-md-12">Frontend development</h2>
+<h2 class="col-md-12">Frontend Development</h2>
 <span  class="ability__block--dev hvr-grow col-xs-12 col-md-12" v-for="ability in abilitiesDev">
 {{ability.name }}
 </span>
 </div>
 
 <div data-aos="fade-up" data-aos-duration="1000"  class="abilities__block col-md-6 col-xs-12">
-<h2>User Experience y gestión de proyectos</h2>
+<h2>User Experience y Gestión de Proyectos</h2>
 <span class="ability__block--ux hvr-grow col-xs-12 col-md-12" v-for="ability in abilitiesUx">
 {{ability.name }}
 </span>
