c0b8ebabe13d6ab95d34800b6d69201e9e982548
HOME <home@MacBook-Pro-de-HOME.local>
2021-05-11 14:40:18 -0500

    New post method have been added

--- test.vue.js
+++ test.vue.js
@@ -3,13 +3,36 @@ const Test = {
         return {
             selection: [],
             mensaje: "this is a test",
+            file:'',
+            newPDF:''
 
         }
     },
     methods: {
-        SendFile() {
+        async SendFile() {
+  
 
-           console.log("HI"); 
+           const API_URL = "http://voltiumlab.com/imptest/back/sendDocument.php";
+ 
+               
+             const response = await fetch(API_URL, {
+               method: "POST",
+               headers: {
+                 "Content-Type": "application/json",
+               },
+               body: JSON.stringify({
+                 text: this.newPDF
+               }),
+             });
+             const json = await response.json();
+             console.log(json);
+             if (response.ok) {
+               props.updateLinkList();
+             } else {
+               console.log("Fail");
+             }
+   
+
         
     },
 
@@ -19,7 +42,7 @@ const Test = {
   <div class="test__body">
 <form id="fileForm" action="back/sendDocument.php" @submit.prevent="SendFile">
   <label for="userfile"> Upload your pdf</label>
-  <input v-model: id="userfile" name="userfile" type="file" accept="application/pdf"/>
+  <input  v-model="newPDF" id="userfile" name="userfile" type="file" accept="application/pdf"/>
   <button> Upload </button>
  </form>
 </div>
