9cf35a30662c38b200e3f98b69438142463943f6
HOME <home@MacBook-Pro-de-HOME.local>
2021-05-11 18:27:10 -0500

    New style have been added

--- test.vue.js
+++ test.vue.js
@@ -3,53 +3,61 @@ const Test = {
         return {
             selection: [],
             mensaje: "this is a test",
-            files:'',
-            newpdf:''
+            files: '',
+            newpdf: ''
 
         }
     },
     methods: {
-        changeFiles(e){
+        changeFiles(e) {
             this.files = e.target.files || e.dataTransfer.files;
             console.log(this.files[0]);
         },
-     
-        async SendFile() {
-            console.log(this.files[0]);
-           const API_URL = "https://voltiumlab.com/imptest/back/sendDocument.php";
-           var data = new FormData()
-           data.append('files', this.files[0])
-             const response = await fetch(API_URL, {
-               method: "POST",
-              /* headers: {
-                 "Content-Type": "application/json",
-               },*/
-               body:
-             // JSON.stringify({ data }),
-               data,
-             });
-             const json = await response.json();
-             console.log(json);
-             if (response.ok) {
-               props.updateLinkList();
-             } else {
-               console.log("Fail");
-             }
-   
-        
-    },
 
-},
+        async SendFile() {
+            const API_URL = "https://voltiumlab.com/imptest/back/sendDocument.php";
+            var data = new FormData()
+            data.append('files', this.files[0])
+            const response = await fetch(API_URL, {
+                method: "POST",
+                /* headers: {
+                   "Content-Type": "application/json",
+                 },*/
+                body:
+                    // JSON.stringify({ data }),
+                    data,
+            });
+            const json = await response.json();
+            console.log(json);
+            if (response.ok) {
+                props.updateLinkList();
+            } else {
+                console.log("Fail");
+            }
+
+
+        },
+
+    },
     template:/* vue-html */ `
-  <div class="test__body">
-  
-<form id="fileForm"  @submit.prevent="SendFile">
-35 {{mensaje}}
-  <label for="userfile"> Upload your pdf</label>
-  <input  id="userfile" @change="changeFiles" name="userfile" type="file" accept="application/pdf"/>
-  <button> Upload </button>
- </form>
-</div>
+    <div class="test__body">
+    
+    <div class="test__body__call-to-action">
+    <h2> Welcome to this Imprimu test </h2>
+    <form class="test__form" id="fileForm" @submit.prevent="SendFile">
+    <label for="userfile"> Upload your pdf to send a email with it</label>
+    <input  id="userfile" @change="changeFiles" name="userfile" type="file" accept="application/pdf"/>
+        <button> Submit </button>
+    </form>
+    </div>
+    
+    <div class="test__body__image">
+
+    image
+    </div>
+
+
+    </div>
 
 `
 };
