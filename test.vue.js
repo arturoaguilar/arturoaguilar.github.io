e36aebabeaa12904713f4a0611573d22c3a39097
HOME <home@MacBook-Pro-de-HOME.local>
2021-05-11 15:22:33 -0500

    new fix

--- test.vue.js
+++ test.vue.js
@@ -3,7 +3,7 @@ const Test = {
         return {
             selection: [],
             mensaje: "this is a test",
-            files:'',
+            files:[],
             newpdf:''
 
         }
@@ -11,21 +11,20 @@ const Test = {
     methods: {
         changeFiles(e){
             var files = e.target.files || e.dataTransfer.files;
-            console.log(files);
+            console.log(files[0]);
         },
      
         async SendFile() {
   
            const API_URL = "https://voltiumlab.com/imptest/back/sendDocument.php";
-
+           var data = new FormData()
+           data.append('file', this.files[0])
              const response = await fetch(API_URL, {
                method: "POST",
                headers: {
                  "Content-Type": "application/json",
                },
-               body: JSON.stringify({
-                 text: this.files[0]
-               }),
+               body: data,
              });
              const json = await response.json();
              console.log(json);
@@ -35,7 +34,6 @@ const Test = {
                console.log("Fail");
              }
    
-
         
     },
 
@@ -44,7 +42,7 @@ const Test = {
   <div class="test__body">
   
 <form id="fileForm"  @submit.prevent="SendFile">
-4 {{mensaje}}
+6 {{mensaje}}
   <label for="userfile"> Upload your pdf</label>
   <input   id="userfile" @change="changeFiles" name="userfile" type="file" accept="application/pdf"/>
   <button> Upload </button>
