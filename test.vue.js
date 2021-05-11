691308850994fe357d27036f553e77af9155f8c5
HOME <home@MacBook-Pro-de-HOME.local>
2021-05-11 13:44:53 -0500

    the form have been added

--- test.vue.js
+++ test.vue.js
@@ -1,26 +1,39 @@
 const Test = {
-	data() {
-		return {
-			selection: [],
-			mensaje: "this is a test",
+    data() {
+        return {
+            selection: [],
+            mensaje: "this is a test",
 
-		}
-	},
-	methods: {
-		clickFunct() {
-			console.log('One of my on');
-		}
-	},
-	created() {
+        }
+    },
+    methods: {
+        SendFile() {
+            fetch('https://voltiumlab.com/imptest/back/sendDocument.php', {
+                method: 'POST', // or 'PUT'
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+    },
+    created() {
 
-	},
-	destroyed() {
-		
+    },
+    destroyed() {
 
-	},
-	template:/* vue-html */ `
+
+    },
+    template:/* vue-html */ `
   <div class="test__body">
-<form action="back/sendDocument.php" @submit.prevent="addNewLink">
+<form action="back/sendDocument.php" @submit.prevent="SendFile">
   <label for="userfile"> Upload your pdf</label>
   <input id="userfile" name="userfile" type="file" accept="application/pdf"/>
   <button> Upload </button>
