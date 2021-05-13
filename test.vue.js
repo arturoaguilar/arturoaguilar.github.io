8b49ca3c41dd11ce6fe87ec0f0662addb64da169
HOME <home@MacBook-Pro-de-HOME.local>
2021-05-13 15:38:35 -0500

    minor fix

--- test.vue.js
+++ test.vue.js
@@ -90,7 +90,7 @@ const Test = {
         <button class="test_form__submit"> Submit </button>
     </form>
 
-    <button class="test__form__design"> Design with canva! </button>
+    <button class="test__form__design"> -Design with canva! </button>
     </div>
     
     <div class="test__body__image-container">
